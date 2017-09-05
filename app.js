280738445f792cdc56459f6dada19462b82a4f70
Chris Birchall <chris.birchall@ovoenergy.com>
2017-09-05 11:17:31 +0100

    heroku stuff

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.get('/repos', function (req, res) {
 
 app.use(express.static('public'))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
